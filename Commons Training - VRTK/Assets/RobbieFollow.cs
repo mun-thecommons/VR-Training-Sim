4adb1ea85c3cfbaced982ffe07f8504b98ac7134
mun-thecommons <iccommons@gmail.com>
2019-06-07 15:11:20 -0230

    Created new follow script, made Robbie hover
    - Added the "Hover" script to Robbie but it will not work with the offset script "RobotController"
    - Attempted new means of following by creating a script called "RobbieFollow" (Did not work but is now able to track Player's direction)
    - Done by Mike

---
+++ Commons Training - VRTK/Assets/RobbieFollow.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RobbieFollow : MonoBehaviour
+{
+    public GameObject thePlayer;
+    public float targetDistance;
+    public float allowedDistance = 5;
+    public GameObject robbie;
+    public float speed;
+    public RaycastHit shot;
+    
+    void Update()
+    {
+        transform.LookAt(thePlayer.transform);
+        if(Physics.Raycast(transform.position,transform.TransformDirection(Vector3.forward), out shot))
+        {
+            targetDistance = shot.distance;
+            if(targetDistance >= allowedDistance)
+            {
+                speed = 0.02f;
+                transform.position = Vector3.MoveTowards(transform.position, thePlayer.transform.position, speed);
+
+            }
+            else
+            {
+                speed = 0;
+            }
+        }
+        
+    }
+}
