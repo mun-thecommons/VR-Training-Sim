f2c7ba4efaf38a79c80d14fa47992d0b06ff39b6
fishbowl <iccommons@gmail.com>
2019-08-06 14:59:27 -0230

    Count Down of Going To CS Desk from MS

---
+++ Commons Training - VRTK/Assets/Scripts/Level.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Level : MonoBehaviour
+{
+    private float timer = 15f;
+    public GameObject countDown;
+    private TextMeshProUGUI countDownText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        countDownText = countDown.GetComponent<TextMeshProUGUI>();
+        countDownText.enabled = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        countDownText.text = "Go To Desk: " + timer;
+        if(timer <= 0f)
+        {
+            countDownText.text = "Out of time";
+            if(timer <= 3f)
+            {
+                countDownText.enabled = false;
+            }
+        }
+    }
+}
