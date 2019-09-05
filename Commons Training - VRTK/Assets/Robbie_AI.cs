a54659109cc600700451784d20cd76de86d68acd
fishbowl <iccommons@gmail.com>
2019-09-05 10:01:35 -0230

    Disabled Robbie_AI
    Robbie AI caused Unity crashes

--- Commons Training - VRTK/Assets/Robbie_AI.cs
+++ Commons Training - VRTK/Assets/Robbie_AI.cs
@@ -6,17 +6,28 @@ public class Robbie_AI : MonoBehaviour
 {
     public Transform player;
     public float rotationSpeed = 3.0f;
-    public float moveSpeed = 3.0f;
+    float moveSpeed;
 
 
     // Update is called once per frame
     void Update()
     {
+        if(Vector3.Distance(transform.position, player.position) >= 3.0f)
+        {
+            moveSpeed = 3.0f;
+        }
+        else
+        {
+            moveSpeed = 0.0f;
+        }
+
         transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(player.position - transform.position),
                 rotationSpeed*Time.deltaTime);
-        while (Vector3.Distance(transform.position, player.position) >= 3.0f)
+
+        while (Vector3.Distance(transform.position, player.position) >= 1.0f)
         {
             transform.position += transform.forward * moveSpeed * Time.deltaTime;
         }
+
     }
 }
