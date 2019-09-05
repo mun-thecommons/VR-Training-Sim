a0a7decadc2244b5cfc4d8d91336ca6650680edd
mun-thecommons <iccommons@gmail.com>
2019-09-05 09:35:15 -0230

    RobbieFollow AI without NavMeshAgent
    - Use the Slerp function
    
    Co-Authored-By: anhminhle4898 <anhminhle4898@users.noreply.github.com>

---
+++ Commons Training - VRTK/Assets/Robbie_AI.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Robbie_AI : MonoBehaviour
+{
+    public Transform player;
+    public float rotationSpeed = 3.0f;
+    public float moveSpeed = 3.0f;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(player.position - transform.position),
+                rotationSpeed*Time.deltaTime);
+        while (Vector3.Distance(transform.position, player.position) >= 3.0f)
+        {
+            transform.position += transform.forward * moveSpeed * Time.deltaTime;
+        }
+    }
+}
