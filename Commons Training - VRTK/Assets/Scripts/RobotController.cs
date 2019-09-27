888432395fcdece5162231310177848859570dd5
mwg223 <50664604+mwg223@users.noreply.github.com>
2019-09-27 11:04:12 -0230

    Creating Tutorial
    Player must now complete series of tasks before being able to move
    Still needs improvement but the foundation is there for tweaking

--- Commons Training - VRTK/Assets/Scripts/RobotController.cs
+++ Commons Training - VRTK/Assets/Scripts/RobotController.cs
@@ -9,6 +9,15 @@ public class RobotController : MonoBehaviour
     public GameObject player;
     public float robbiePlayerDistance = 2f;
 
+    // Tutorial Variables
+    private bool movement;
+    private bool mainMenu;
+    private bool direction;
+    private bool staplerShot;
+    private bool clientInteract;
+    private bool tutorialFinished;
+
+
     private AudioSource source;
     public AudioClip welcomeAudio;
     public AudioClip level1CompleteAudio;
@@ -33,6 +42,14 @@ public class RobotController : MonoBehaviour
 
     void Start()
     {
+        // Used for Tutorial
+        movement = false;                   //Bool checklist for the player to complete
+        mainMenu = false;
+        direction = false;
+        staplerShot = false;
+        clientInteract = false;
+        // End
+
         robbieStartPostion = transform.position;
         source = GetComponent<AudioSource>();
     }
@@ -41,9 +58,20 @@ public class RobotController : MonoBehaviour
     void Update()
     {
         //DisplayMessage();
-        DisplayLevelMessage();
-    }
 
+        //Tutorial portion
+        if (!tutorialFinished)
+        {
+            DisplayTutorialMessage();               // Setup as if's to ensure the player must complete tutorial first before level
+            CheckTutorialControls();                // scripts begin to show up on the canvas
+        }
+
+        else
+        {
+            MasterController.EnableMovement();
+            DisplayLevelMessage();
+        }
+    }
     void Teleport()
     {
         transform.position = new Vector3(player.transform.position.x + player.transform.forward.x*robbiePlayerDistance, player.transform.position.y, player.transform.position.z + player.transform.forward.z*robbiePlayerDistance);
@@ -98,6 +126,81 @@ public class RobotController : MonoBehaviour
             level3CompleteMessage = true;
             source.PlayOneShot(level3CompleteAudio, volume);
         }           
+    }
+
+    void DisplayTutorialMessage() 
+    {   
+        /*Will run through what Bool's are false to determine next message to display
+            Should run Chronilogically if i set it up correctly
+          */
+        if (movement)
+        {
+            MasterController.DisableMovement();
+        }
+        if (movement == false)
+        {
+            robotCanvasText.text = "Welcome to your first Tutorial. To start, use the Left Joystick to control your movement. Go ahead, try it!";
+        }
+        else if ( direction == false && movement)
+        {
+            robotCanvasText.text = "Good job, now use the Rigt Joystick to change your direction. Go ahead, try it!";
+        }
+        else if (mainMenu == false && movement && direction)
+        {
+            robotCanvasText.text = "Look at you, you'll be a pro in no time. Now press in on the Left Joystick to bring up your Main Menu. To exit simply press the button again.";
+        }
+        else if (staplerShot == false && movement && direction && mainMenu)
+        {
+            robotCanvasText.text = "While aboard the ship you may need to throw staplers to achieve certain goals. To do this press down on the B button on the right controller.";
+        }
+        else if (clientInteract == false && movement && direction && mainMenu && staplerShot)
+        {
+            robotCanvasText.text = "Finally, while aboard the ship you'll notice some people wil be looking for you're help. These individuals will have ! above their heads. To help these people press the A button.";
+        }
+
+    }
+
+    void CheckTutorialControls()
+    {/* Checks what is being displayed on robbie's canvas to determine which Input.Getbuttondown to look for.
+        After detecting the correct button has been pressed, will set the apropriate bool to true and hence
+        set the TutorialMessage to the next step.
+         */
+        if (robotCanvasText.text == "Welcome to your first Tutorial. To start, use the Left Joystick to control your movement. Go ahead, try it!" )
+        {
+            if (OVRInput.Get(OVRInput.Button.PrimaryThumbstickUp) || OVRInput.Get(OVRInput.Button.PrimaryThumbstickDown) || OVRInput.Get(OVRInput.Button.PrimaryThumbstickRight) || OVRInput.Get(OVRInput.Button.PrimaryThumbstickLeft)) 
+            {
+                movement = true;
+            }
+        }
+        if (robotCanvasText.text == "Good job, now use the Rigt Joystick to change your direction. Go ahead, try it!")
+        {
+            if (OVRInput.Get(OVRInput.Button.SecondaryThumbstickUp) || OVRInput.Get(OVRInput.Button.SecondaryThumbstickDown) || OVRInput.Get(OVRInput.Button.SecondaryThumbstickRight) || OVRInput.Get(OVRInput.Button.SecondaryThumbstickLeft))           
+            {
+                direction = true;
+            }
+        }
+        if (robotCanvasText.text == "Look at you, you'll be a pro in no time. Now press in on the Left Joystick to bring up your Main Menu. To exit simply press the button again.")
+        {
+            if (OVRInput.GetDown(OVRInput.RawButton.LThumbstick))
+            {
+                mainMenu = true;
+            }
+        }
+        if (robotCanvasText.text == "While aboard the ship you may need to throw staplers to achieve certain goals. To do this press down on the B button on the right controller.")
+        {
+            if (OVRInput.GetDown(OVRInput.RawButton.B))
+            {
+                staplerShot = true;
+            }
+        }
+        if (robotCanvasText.text == "Finally, while aboard the ship you'll notice some people wil be looking for you're help. These individuals will have ! above their heads. To help these people press the A button.")
+        {
+            if (OVRInput.GetDown(OVRInput.RawButton.A))
+            {
+                clientInteract = true;
+                tutorialFinished = true;
+            }
+        }
     }
 
 }
