25258816049e4ec618cd293b0095633eea3b9a83
mwg223 <50664604+mwg223@users.noreply.github.com>
2019-11-01 13:07:17 -0230

    Organization Changes
    -Asset packages brought in from the unity store and 3rd party sources have been put into a Folder called "Game Assets".
    
    -Also put all of our Sketchup and Fusion imported models into one folder named "Fusion_Sketchup Imports" and this folder was placed within the Game Assets folder
    
    -Prefabs have also been categorized
    
    -Scripts has been organized into subcategories based on the purpose of the script
    
    -Multiple Unused assets and Materials have been removed
    
    -A number of Tags have been removed

---
+++ Commons Training - VRTK/Assets/Scripts/Alien_Scripts/AlienController.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AlienController : MonoBehaviour
+{
+    private float yPos = 0.3f;
+    static public List<Vector3> walkDestPos = new List<Vector3>();
+    private int destPoint = 0;
+    private NavMeshAgent agent;
+    private GameObject[] monitors;
+    private Animator alienAnimator;
+    public GameObject whiteSmokePrefab;
+    private GameObject whiteSmoke;
+
+    void Start()
+    {
+        monitors = GameObject.FindGameObjectsWithTag("Monitor");
+        alienAnimator = gameObject.GetComponent<Animator>();
+
+        walkDestPos.Add(new Vector3(0.0f, yPos, -3.6f));
+        walkDestPos.Add(new Vector3(9.7f, yPos, -12.0f));
+        walkDestPos.Add(new Vector3(3.9f, yPos, -12.0f));
+        walkDestPos.Add(new Vector3(3.9f, yPos, -29.3f));
+        walkDestPos.Add(new Vector3(29.1f, yPos, -20.6f));
+        walkDestPos.Add(new Vector3(-12.9f, yPos, -23.3f));
+        walkDestPos.Add(new Vector3(-27.0f, yPos, -23.3f));
+        walkDestPos.Add(new Vector3(-36.7f, yPos, -8.1f));
+        walkDestPos.Add(new Vector3(-17.6f, yPos, 18.2f));
+        walkDestPos.Add(new Vector3(1.4f, yPos, 18.2f));
+        walkDestPos.Add(new Vector3(5.5f, yPos, 7.0f));
+
+        agent = GetComponent<NavMeshAgent>();
+        agent.autoBraking = false;
+        destPoint = Random.Range(0, walkDestPos.Count);
+    }
+
+    void GotoNextPoint()
+    {
+        if (walkDestPos.Count == 0)
+            return;
+        agent.destination = walkDestPos[destPoint];
+        destPoint = (destPoint + 1) % walkDestPos.Count;
+    }
+
+    void Update()
+    {
+        if (!agent.pathPending && agent.remainingDistance < 0.5f)
+        {
+            GotoNextPoint();
+        }
+        monitors = GameObject.FindGameObjectsWithTag("Monitor");
+        foreach (GameObject i in monitors)
+        {
+            if(Vector3.Distance(gameObject.transform.position, i.transform.position) <= 2.5f)
+            {
+                transform.LookAt(i.transform.position);
+                alienAnimator.SetTrigger("Shoot");
+                whiteSmoke = Instantiate(whiteSmokePrefab, i.transform.position, i.transform.rotation);
+                Destroy(i);
+                Object.Destroy(whiteSmoke, 4.0f);
+            }
+        }
+    }
+}
