c9e4a8e8839a6e4506074ce35803852d4fd67173
mwg223 <50664604+mwg223@users.noreply.github.com>
2019-11-08 13:07:52 -0330

    Cashbox Client Rework
    -Updated Cashbox Client into the UMA Model
    -Attempting to update the script into something we can use for the current models
    - Having issues with enabling/disabling mesh renderer
    - Interaction canvas is having issues working with more then one client as well

--- Commons Training - VRTK/Assets/Scripts/Player_Client_Interaction/TestAnimatorController.cs
+++ Commons Training - VRTK/Assets/Scripts/Player_Client_Interaction/TestAnimatorController.cs
@@ -9,6 +9,8 @@ public class TestAnimatorController : MonoBehaviour
     Animator animator;
     public Vector3 goal;
     public Vector3 deskGoal;
+    public Vector3 cashGoal;
+
     // Use this for initialization
     void Start()
     {
