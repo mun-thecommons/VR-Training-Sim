1a92e41d0f04a6e40ff3290e02060ffe3e4c6a0b
mun-thecommons <iccommons@gmail.com>
2020-02-09 15:25:06 -0330

    Added Door Animations

---
+++ Commons Training - VRTK/Assets/Scripts/Player_Environment_Interaction/DoorController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorController : MonoBehaviour
+{
+
+    bool isOpen = false;
+
+    private Animator doorAnim;
+    private Collider[] colliders;
+
+    private AudioSource doorAudio;
+    public AudioClip doorOpen;
+
+    void Start()
+    {
+        doorAnim = GetComponent<Animator>();
+        colliders = GetComponents<Collider>();
+        doorAudio = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Hand"))
+        {
+            // Toggle state of IsOpen Bool, trigger the button
+            doorAnim.SetBool("IsOpen", !doorAnim.GetBool("IsOpen"));
+            doorAnim.SetTrigger("ButtonPress");
+            doorAudio.PlayOneShot(doorOpen);
+        }
+    
+    }
+}
